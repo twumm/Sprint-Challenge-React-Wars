240bae9487ae162a7d05954dee491814e49b9343
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-31 10:24:30 +0000

    feature: Add method to get previous and next list

--- starwars/src/components/StarWarsDisplay.js
+++ starwars/src/components/StarWarsDisplay.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import StarWarsList from './StarWarsList';
 
-export default function StarWarsDisplay({ starWarsChars }) {
+export default function StarWarsDisplay(
+  { 
+    starWarsChars,
+    nextListUrl,
+    previousListUrl,
+    getCharacters
+  }) {
+
+  const onGetCharacters = (event, URL) => {
+    getCharacters(URL)
+  };
+
   return (
     <div>
       <div className="character-display">
@@ -10,12 +21,16 @@ export default function StarWarsDisplay({ starWarsChars }) {
       <div className="pagination-buttons">
         <button
           className="previous-button"
+          style={!previousListUrl ? {display: 'none'} : null }
+          onClick={(event) => onGetCharacters(event, previousListUrl)}
         >
           Previous
         </button>
 
         <button
           className="previous-button"
+          style={!nextListUrl ? {display: 'none'} : null}
+          onClick={(event) => onGetCharacters(event, nextListUrl)}
         >
           Next
         </button>
