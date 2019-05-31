9fe19927f04924c6679fc166da5931e85fb1c1f4
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-31 09:05:38 +0000

    feature: Use font awesome to display gender

--- starwars/src/components/StarWarsList.js
+++ starwars/src/components/StarWarsList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './StarWars.css';
 
 export default function StarWarsList({  starWarsChars }) {
@@ -8,7 +9,15 @@ export default function StarWarsList({  starWarsChars }) {
         <div key={character.created} className="character-card">
           <div className="character-header">
             <h4 id="character-name">{character.name}</h4>
-            <span id="character-gender">{character.gender}</span>
+            <span id="character-gender">
+              {
+                character.gender === 'male'
+                ? <FontAwesomeIcon icon="male" size="2x" color="#89cff0" />
+                : character.gender === 'female'
+                ? <FontAwesomeIcon icon="female" size="2x" color="pink" />
+                : <FontAwesomeIcon icon="genderless" size="2x" color="black" spin />
+              }
+            </span>
           </div>
           <div className="character-stats">
             <span>{character.eye_color}</span>
