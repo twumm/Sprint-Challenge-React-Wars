e0db3a32c7937e503145418f5b22aa6dbf291d3f
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-31 19:04:28 +0000

    Revert "WEBEU2 - Martin Mensah - Anthony Campbell - Week 06 Day 05"

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
-import StarWarsDisplay from './components/StarWarsDisplay';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faMale, faFemale, faGenderless, faWeight } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
-
-library.add(faMale, faFemale, faGenderless, faWeight);
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: [],
-      nextList: '',
-      previousList: '',
-      onLoading: false
+      starwarsChars: []
     };
   }
 
@@ -27,39 +19,20 @@ class App extends Component {
     // We then take that data and resolve it our state.
     fetch(URL)
       .then(res => {
-        this.toggleLoading()
         return res.json();
       })
       .then(data => {
-        this.setState({
-          starwarsChars: data.results,
-          nextList: data.next,
-          previousList: data.previous,
-          onLoading: false,
-        });
+        this.setState({ starwarsChars: data.results });
       })
       .catch(err => {
         throw new Error(err);
       });
   };
 
-  toggleLoading = () => {
-    this.setState({
-      onLoading: true
-    })
-  }
-
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <StarWarsDisplay
-          starWarsChars={this.state.starwarsChars}
-          nextListUrl={this.state.nextList}
-          previousListUrl={this.state.previousList}
-          getCharacters={this.getCharacters}
-          onLoading={this.state.onLoading}
-        />
       </div>
     );
   }
